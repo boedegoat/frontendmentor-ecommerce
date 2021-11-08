aaa5bc95c6d64a6d0ac7a5b1eb4a6699f63e4ec5
boedegoat <bhremada.fka@gmail.com>
2021-11-08 15:19:15 +0700

    Add icon and meta tags

--- components/Container.js
+++ components/Container.js
@@ -1,11 +1,14 @@
 import Head from 'next/head'
 import Navbar from './Navbar'
+import product from 'data/product'
 
 export default function Container({ children }) {
   return (
     <>
       <Head>
-        <title>🛒 Ecommerce Product Page</title>
+        <title>Sneakers - {product.name}</title>
+        <meta name='description' content={product.description} />
+        <link rel='icon' type='image/png' href='/images/favicon-32x32.png'></link>
       </Head>
       <Navbar />
       <main>{children}</main>
