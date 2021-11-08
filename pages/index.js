ea86806391508585b4a8fa96869fe3fb80044fde
boedegoat <bhremada.fka@gmail.com>
2021-11-08 08:29:55 +0700

    Filling tailwind.config.js with custom colors and fontFamily

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <div className='text-2xl font-bold'>samlekom</div>
+  return <div className='text-5xl text-app-orange'>Samlekom</div>
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,28 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   mode: 'jit',
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["'Kumbh Sans', sans-serif", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        app: {
+          orange: 'hsl(26, 100%, 55%)',
+          'pale-orange': 'hsl(25, 100%, 94%)',
+          blue: {
+            400: 'hsl(220, 13%, 13%)',
+            300: 'hsl(219, 9%, 45%)',
+            200: 'hsl(220, 14%, 75%)',
+            100: 'hsl(223, 64%, 98%)',
+          },
+          'black-opacity-75': 'hsl(0, 0%, 0%)',
+        },
+      },
+    },
   },
   variants: {
     extend: {},
