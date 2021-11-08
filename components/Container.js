947a7bfbfe6919aaf1fc8cb68ae8d6577f2f9531
boedegoat <bhremada.fka@gmail.com>
2021-11-08 16:31:45 +0700

    Edit app title to use product.name only

--- components/Container.js
+++ components/Container.js
@@ -6,7 +6,7 @@ export default function Container({ children }) {
   return (
     <>
       <Head>
-        <title>Sneakers - {product.name}</title>
+        <title>{product.name}</title>
         <meta name='description' content={product.description} />
         <link rel='icon' type='image/png' href='/images/favicon-32x32.png'></link>
       </Head>
