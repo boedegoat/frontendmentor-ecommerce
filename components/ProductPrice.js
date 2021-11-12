87ab7388437b86e3e16363dada78c356339315b2
boedegoat <bhremada.fka@gmail.com>
2021-11-12 09:46:52 +0700

    Make product price style for desktop

--- components/ProductPrice.js
+++ components/ProductPrice.js
@@ -3,12 +3,12 @@ import { formatPrice } from 'lib/utils'
 
 export default function ProductPrice() {
   return (
-    <div className='wrapper flex items-center py-2'>
+    <div className='wrapper flex md:flex-wrap items-center py-2'>
       <h2 className='font-bold text-3xl'>{formatPrice(product.price.afterDiscount)}</h2>
       <span className='font-bold text-app-orange bg-app-pale-orange rounded-md text-sm px-2 py-1 ml-2'>
         {product.discount}
       </span>
-      <p className='ml-auto font-bold text-app-blue-200 line-through'>
+      <p className='ml-auto font-bold text-app-blue-200 line-through md:w-full md:mt-2'>
         {formatPrice(product.price.normal)}
       </p>
     </div>
